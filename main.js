c65ade48a68087bf24e6f282803ed9f0bc0bfe96
lraj22 <63673413+lraj22@users.noreply.github.com>
2024-12-09 21:10:56 -0800

    Add time view functionality

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+// main.js
+// the logic of the operation!
+
+// variablization of all [id]s
+document.querySelectorAll("[id]").forEach(function (e) { window[e.id] = e; });
+
+function tick () {
+	// update time view
+	var d = new Date();
+	var hours = (d.getHours() % 12) || 12;
+	var minutes = d.getMinutes().toString().padStart(2, "0");
+	timeView.textContent = hours + ":" + minutes;
+	
+	// to next tick
+	requestAnimationFrame(tick);
+}
+
+requestAnimationFrame(tick);
